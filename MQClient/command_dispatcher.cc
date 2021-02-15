0cae9517806a55f6c6a338b7cf0bc7e164abac3d
Lucas Vieira <lucas.engen.cc@gmail.com>
2021-02-14 23:48:11 -0300

    fix: :bug: memory corruption while passing data to command processor
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- MQClient/command_dispatcher.cc
+++ MQClient/command_dispatcher.cc
@@ -47,40 +47,29 @@ void CommandDispatcher::Dispatch()
 {
     MQTTClient_message *msg = static_cast<MQTTClient_message*>(message);
 
-    // Check command size
-    if(msg->payloadlen > MAX_CMD_SIZE) {
-        Log::LogError("Too big command size: " + to_string(msg->payloadlen));
-        auto c = static_cast<MqttClient*>(this->client);
-        c->Publish("/error", this->topic);
+    if(msg->payloadlen <= 0) {
+        MQTTClient_freeMessage(&msg);
+        Log::LogError("Invalid payload length");
         return;
     }
 
-    allocator<char> a1;
-
-    auto buff = a1.allocate(msg->payloadlen);
-
-    if(!buff) {
-        Log::LogPanic("Failed to allocate memory");
+    // Check command size
+    if(msg->payloadlen > MAX_CMD_SIZE) {
+        Log::LogError("Too big command size: " + to_string(msg->payloadlen));
+        printf("command: %s\n", (char*)msg->payload);
+        static_cast<MqttClient*>(this->client)->SendError();
+        return;
     }
 
-    // Initialize memory
-    memset(buff, 0, msg->payloadlen);
+    // Setup command processor
+    auto cp = new CommandProcessor(static_cast<char*>(msg->payload), static_cast<MqttClient*>(this->client));
 
-    // Copy payload into buffer
-    memcpy(buff, msg->payload, msg->payloadlen);
-
-    string command = string(reinterpret_cast<char*>(msg->payload));
-    CommandProcessor cp(command, static_cast<MqttClient*>(this->client));
-
-    try {
-
-        // Analyse command syntax
-        cp.Process();
-    } catch(std::runtime_error& re) {
-        printf("Runtime error detected: %s\n", re.what());
-    }
+    // Analyse command syntax
+    cp->Process();
 
     // Free used memory
     MQTTClient_freeMessage(&msg);
-    a1.deallocate(buff, msg->payloadlen);
+
+    if(cp)
+        delete cp;
 }

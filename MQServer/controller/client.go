370c1bea6168b3e0d8014467be8130a54243ec16
Lucas Vieira <lucas.engen.cc@gmail.com>
2021-02-23 02:12:13 -0300

    refactor: :recycle: completely refactored client handler
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- MQServer/controller/client.go
+++ MQServer/controller/client.go
@@ -1,5 +1,52 @@
 package controller
 
-func HandleClient(clientID string) {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+// Commands stores all command pending
+var Commands chan string
+
+// CommandProcessed channel is updated whenever a command is sent to client
+var CommandProcessed = make(chan bool, 1)
+
+// ClientConnected is updated whenever a client connects to server
+var ClientConnected = make(chan bool, 1)
+
+func setupCommander() {
+	Commands = make(chan string, 1)
+}
+
+// ReadCommands function read a command and dispatch to all connected clients
+func ReadCommands() {
+
+	// Configure commander
+	setupCommander()
+
+	// Wait some client to be connected
+	fmt.Println("Waiting for clients ...")
+
+	<-ClientConnected
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Printf("shell> ")
+		command, fail := reader.ReadString('\n')
+		if fail != nil {
+			fmt.Printf("[ERROR] Failed to read command: %v\n", fail)
+			continue
+		}
+
+		// Remove whitespace
+		command = command[:len(command)-2]
+
+		// Insert command to channel
+		Commands <- command
+
+		// Wait for command to be processed
+		<-CommandProcessed
+	}
 }

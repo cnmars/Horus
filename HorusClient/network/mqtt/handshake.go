8b99b2a57c9828f2ab38431d0b0a154030b60fe5
Lucas Vieira <lucas.engen.cc@gmail.com>
2022-04-01 04:13:19 -0300

    refactor: :recycle: uses internal variable
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- HorusClient/network/mqtt/handshake.go
+++ HorusClient/network/mqtt/handshake.go
@@ -1,15 +1,14 @@
 package mqtt
 
 import (
-	"HorusClient/model"
 	"fmt"
 )
 
-const handshakeReq string = "hello world"
+const handshakeReq string = "/hello"
 
 func sendHandshake() {
 
-	topicName := fmt.Sprintf("%v/%v/%v", model.GetTopicNameByID(model.TopicIDBase), customClient.ID, model.GetTopicNameByID(model.TopicIDHs))
+	topicName := fmt.Sprintf("%v", customClient.HandshakeTopic)
 
 	// Publish a message into handshake topic to notify server
 	token := client.Publish(topicName, 0, false, handshakeReq)
